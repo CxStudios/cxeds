6c46e755fe6193fff1e7bd12dc49f6b05c34a729
mebrihit-z <mebrihit@cxstudios.io>
2025-03-28 18:07:16 -0400

    Worked on partner with us to

--- blocks/what-we-do/what-we-do.js
+++ blocks/what-we-do/what-we-do.js
@@ -1,6 +1,3 @@
-import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
-const placeholders = await fetchPlaceholders(getMetadata("locale"));
-
 export default function decorate(block) {
     const rows= [...block.children];
     [...block.children].forEach((row,r) => {
